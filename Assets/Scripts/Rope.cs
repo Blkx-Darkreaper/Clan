3eb17d150821fe9e44aabcad7249d22d0f894b46
Nicolaj Bunting <steelmagnum@hotmail.com>
2020-03-31 09:53:59 -0700

    Initial commit of Clan unity project

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    protected LineRenderer lineRenderer;
+    protected List<RopeSegment> allSegments = new List<RopeSegment>();
+
+    public struct LineSegment
+    {
+        public Vector2 currentPosition;
+        public Vector2 previousPosition;
+
+        public RopeSegment(Vector2 position)
+        {
+            this.currentPosition = position;
+            this.previousPosition = position;
+        }
+    }
+}
