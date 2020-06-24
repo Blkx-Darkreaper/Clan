1504f04eac3939019fcec75856e3f8f0cc6051a6
Nicolaj Bunting <steelmagnum@hotmail.com>
2020-06-24 11:01:33 -0700

    Restructured folders

---
+++ Assets/Base/Scripts/EventDataArg.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class EventDataArg<T> : EventArgs
+{
+    public T Value { get { return Value; } }
+    protected T value;
+
+    public EventDataArg(T value)
+    {
+        this.value = value;
+    }
+}
---
+++ Assets/Base/Scripts/GameEventListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent gameEvent;
+    public UnityEvent response;
+
+    protected virtual void OnEnable()
+    {
+        gameEvent.RegisterListener(this);
+    }
+
+    protected virtual void OnDisable()
+    {
+        gameEvent.UnregisterListener(this);
+    }
+
+    public virtual void OnEventRaised()
+    {
+        response.Invoke();
+    }
+}
