4d42bcc01fcc872ed7786c6f1a0f6bafa1dac212
Nicolaj Bunting <steelmagnum@hotmail.com>
2020-06-26 08:04:22 -0700

    Added tilemap script extensions

---
+++ Assets/Base/Tilemap/Tiles/Animated Rule Tile/Scripts/Editor/CustomAnimatedRuleTileMenu.cs
@@ -0,0 +1,11 @@
+﻿namespace UnityEditor
+{
+    static class CustomAnimatedRuleTileMenu
+    {
+        [MenuItem("Assets/Create/Custom Animated Rule Tile Script", false, 89)]
+        static void CreateCustomAnimatedRuleTile()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile("Assets/Tilemap/Tiles/Animated Rule Tile/ScriptTemplates/NewCustomAnimatedRuleTile.cs.txt", "NewCustomAnimatedRuleTile.cs");
+        }
+    }
+}
